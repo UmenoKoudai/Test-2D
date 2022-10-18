a495c51c853fb48c017a27c2ba790ec555bb0f94
umenokoudai <umenokoudai0805@gmail.com>
2022-10-18 22:39:36 +0900

    no message

---
+++ Assets/Player.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] GameObject _bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+}
